b000009ec7655d68561d2a067a4f0761bdaa1c6d
Mess <Messi>
2019-12-24 11:33:30 -0800

    Got hashing working

--- dup-file-finder.py
+++ dup-file-finder.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 
 import sys
+import os
+import hashlib
 from pathlib import Path
+
+
 
 folder = "./"
 
 if(len(sys.argv) > 1):
     folder = sys.argv[1]
 
-result = list(Path(folder).rglob("*.*"))
-print(result[1])
+paths = list(Path(folder).rglob("*.*"))
+hashes = {}
+
+for path in paths:
+    if os.path.isfile(path):
+        fileText = Path(path).read_text()
+        fileHash = hashlib.md5(fileText.encode())
+        hashes[path] = fileHash
+
+print(hashes)
