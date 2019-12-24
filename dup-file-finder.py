fd243f5a643f902cd96028cf7b0e0dbe06933e77
Mess <Messi>
2019-12-24 12:24:43 -0800

    changes

--- dup-file-finder.py
+++ dup-file-finder.py
@@ -5,6 +5,17 @@ import os
 import hashlib
 from pathlib import Path
 
+def findDuplicateDictValues(dictionary):
+    for key, value in dictionary.items():
+        for key2, value2 in dictionary.items():
+            print(value + ", " + value2)
+            print(key + ", " + key2)
+            print(key != key2)
+            print(value == value2)
+            
+            if key != key2 and value == value2:
+                print(key + ", " + key2)
+
 
 
 folder = "./"
@@ -19,6 +30,6 @@ for path in paths:
     if os.path.isfile(path):
         fileText = Path(path).read_text()
         fileHash = hashlib.md5(fileText.encode())
-        hashes[path] = fileHash
+        hashes[str(path)] = str(fileHash)
 
-print(hashes)
+findDuplicateDictValues(hashes)
