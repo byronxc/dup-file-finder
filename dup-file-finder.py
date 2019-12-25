97b8ba23bb19b2dd7fb2b5a772ef9264610b932e
Mess <Messi>
2019-12-24 20:02:08 -0800

    text file error

--- dup-file-finder.py
+++ dup-file-finder.py
@@ -8,10 +8,10 @@ from pathlib import Path
 def findDuplicateDictValues(dictionary):
     for key, value in dictionary.items():
         for key2, value2 in dictionary.items():
-            print(value + ", " + value2)
-            print(key + ", " + key2)
-            print(key != key2)
-            print(value == value2)
+            # print(value + ", " + value2)
+            # print(key + ", " + key2)
+            # print(key != key2)
+            # print(value == value2)
             
             if key != key2 and value == value2:
                 print(key + ", " + key2)
@@ -30,6 +30,6 @@ for path in paths:
     if os.path.isfile(path):
         fileText = Path(path).read_text()
         fileHash = hashlib.md5(fileText.encode())
-        hashes[str(path)] = str(fileHash)
+        hashes[str(path)] = str(fileText)
 
 findDuplicateDictValues(hashes)
